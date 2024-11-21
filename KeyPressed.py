aad1c5159d2351e890489292aaa15cf8468bc35d
LagBot5 <167463120+LagBot5@users.noreply.github.com>
2024-11-21 10:09:39 -0500

    Update KeyPressed.py

--- KeyPressed.py
+++ KeyPressed.py
@@ -1,8 +1,8 @@
-function keyPressed(){
+function keyPressed() {
 	// let distance = p5.Vector.dist( pacman.Pos, GenerateList[pacman.GridPos].Pos)
 	// d
-	if (keyCode === 68 || keyCode === RIGHT_ARROW){
-		if (GenerateList[pacman.GridPos + 1].Collide == false ){
+	if (keyCode === 68 || keyCode === RIGHT_ARROW) {
+		if (GenerateList[pacman.GridPos + 1].Collide == false) {
 			pacman.Pos.x = GenerateList[pacman.GridPos].Pos.x
 			pacman.Pos.y = GenerateList[pacman.GridPos].Pos.y
 			pacman.Xspeed = pacman.Speed
@@ -13,11 +13,11 @@ function keyPressed(){
 		}
 	}
 	// a
-	else if (keyCode === 65 || keyCode === LEFT_ARROW){
-		if (GenerateList[pacman.GridPos - 1].Collide == false){
+	else if (keyCode === 65 || keyCode === LEFT_ARROW) {
+		if (GenerateList[pacman.GridPos - 1].Collide == false) {
 			pacman.Pos.x = GenerateList[pacman.GridPos].Pos.x
 			pacman.Pos.y = GenerateList[pacman.GridPos].Pos.y
-			pacman.Xspeed = pacman.Speed*-1
+			pacman.Xspeed = pacman.Speed * -1
 			pacman.Yspeed = 0
 			pacman.AngleA = West
 			pacman.AngleB = West + 360
@@ -25,8 +25,8 @@ function keyPressed(){
 		}
 	}
 	// s
-	else if (keyCode === 83 || keyCode === DOWN_ARROW){
-		if (GenerateList[pacman.GridPos + 21].Collide == false){
+	else if (keyCode === 83 || keyCode === DOWN_ARROW) {
+		if (GenerateList[pacman.GridPos + 21].Collide == false) {
 			pacman.Pos.x = GenerateList[pacman.GridPos].Pos.x
 			pacman.Pos.y = GenerateList[pacman.GridPos].Pos.y
 			pacman.Xspeed = 0
@@ -37,25 +37,24 @@ function keyPressed(){
 		}
 	}
 	// w
-	else if (keyCode === 87 || keyCode === UP_ARROW){
-		if (GenerateList[pacman.GridPos - 21].Collide == false){
+	else if (keyCode === 87 || keyCode === UP_ARROW) {
+		if (GenerateList[pacman.GridPos - 21].Collide == false) {
 			pacman.Pos.x = GenerateList[pacman.GridPos].Pos.x
 			pacman.Pos.y = GenerateList[pacman.GridPos].Pos.y
 			pacman.Xspeed = 0
-			pacman.Yspeed = pacman.Speed*-1
+			pacman.Yspeed = pacman.Speed * -1
 			pacman.AngleA = North
 			pacman.AngleB = North + 360
 			pacman.Direction = North
 		}
-	}
-	else if (keyCode === 70){
+	} else if (keyCode === 70) {
 		pacman.Xspeed = 0
 		pacman.Yspeed = 0
 	}
-	if (keyIsDown(80)){
+	if (keyIsDown(80)) {
 		points(50)
 	}
-	if (keyIsDown(79)){
+	if (keyIsDown(79)) {
 		points(200)
 	}
 }
